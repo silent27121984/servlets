5a114c9113fe940bfe7c37c5ef8771e3876422a1
silent27121984 <sergey27121984@gmail.com>
2023-06-22 12:19:48 +0300

    feature/di-java

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,7 +1,7 @@
 package ru.netology.servlet;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,9 +16,8 @@ public class MainServlet extends HttpServlet {
     static private final String PATH_FOR_REMOVE_POST = "/api/posts/\\d+";
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        controller = context.getBean(PostController.class);
     }
 
     @Override
