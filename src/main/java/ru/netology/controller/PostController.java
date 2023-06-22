05a37a6b922d0fade1316ac5d61948d60f2de7a1
silent27121984 <sergey27121984@gmail.com>
2023-06-22 13:15:57 +0300

    feature/di-java

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -3,12 +3,10 @@ package ru.netology.controller;
 import com.google.gson.Gson;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
-import org.springframework.stereotype.Controller;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Reader;
 
-@Controller
 public class PostController {
     public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
